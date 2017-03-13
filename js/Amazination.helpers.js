b37f4fe4856557abc7da5326c04dc40b24c08851
Charles Mangwa <charles.mangwa-chuisse@hetic.net>
2017-03-13 10:58:18 +0100

    refactor(*): minor refactoring

--- js/Amazination.helpers.js
+++ js/Amazination.helpers.js
@@ -5,47 +5,43 @@ type AnimationDirections = 'slideUp' | 'slideDown' | 'slideLeft' | 'slideRight'
 type AnimationOpacity = 'fadeIn' | 'fadeOut'
 
 type AnimationOptions = null | {
-  delay?: number,
-  duration?: number,
-  easing?: string,
   offset?: 'bottom-in-view' | number | string | Function,
   opacity?: number,
   scale?: number,
+  timing?: {
+    delay?: number,
+    duration?: number,
+    easing?: string,
+  },
   translateX?: number,
 }
 
 const getFadeOptions = (animation: AnimationOpacity, targets: string, options: AnimationOptions) => {
-  const { delay, duration, easing, offset, opacity: opacityValue } = options
+  const { offset, opacity: opacityValue, timing } = options
   const defaultOpacity = Number(animation === 'fadeIn')
   return {
+    ...timing,
     targets,
-    delay: delay || 0,
-    duration: duration || 300,
-    easing: easing || 'linear',
     opacity: opacityValue || defaultOpacity,
   }
 }
 
 const getSlideOptions = (direction: AnimationDirections, targets: string, options: AnimationOptions) => {
-  const { delay, duration, easing, offset, translate } = options
+  const { offset, translate, timing } = options
   const translateValue = direction === 'slideLeft' | direction === 'slideUp' ? -translate : translate
   return {
+    ...timing,
     targets,
-    delay: delay || 0,
-    duration: duration || 300,
-    easing: easing || 'linear',
     translateX: direction === 'slideLeft' || direction === 'slideRight' ? translateValue : 0,
     translateY: direction === 'slideUp' || direction === 'slideDown' ? translateValue : 0,
   }
 }
 
 const getScaleOptions = (targets: string, options: AnimationOptions) => {
-  const { delay, duration, easing, offset, scale: scaleValue } = options
+  const { offset, scale: scaleValue, timing } = options
   return {
+    ...timing,
     targets,
-    delay: delay || 0,
-    duration: duration || 300,
-    easing: easing || 'linear',
     scale: scaleValue || 1.2,
   }
 }
--- js/Amazination.js
+++ js/Amazination.js
@@ -14,17 +14,25 @@ type AnimationType = 'fadeIn' | 'fadeOut' | 'slideLeft' | 'slideRight' | 'scale'
 
 class Amazination {
   renderAnimation(elements: string, animation: AnimationType, options: AnimationOptions) {
+    const parameters = {
+      default: {
+        delay: options.delay || 0,
+        duration: options.duration || 300,
+        easing: options.easing || 'linear',
+      },
+      ...options,
+    }
     switch (animation) {
       case 'fadeIn':
       case 'fadeOut':
-        return anime(getFadeOptions(animation, elements, options))
+        return anime(getFadeOptions(animation, elements, parameters))
       case 'scale':
-        return anime(getScaleOptions(elements, options))
+        return anime(getScaleOptions(elements, parameters))
       case 'slideUp':
       case 'slideDown':
       case 'slideLeft':
       case 'slideRight':
-        return anime(getSlideOptions(animation, elements, options))
+        return anime(getSlideOptions(animation, elements, parameters))
       default:
         return false
     }
